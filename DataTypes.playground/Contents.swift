33754db831b2682ecd44d53fccd346a472239746
jicaey <young.ma@me.com>
2016-08-18 16:30:24 -0400

    Completed Problems

---
+++ DataTypes.playground/Contents.swift
@@ -0,0 +1,17 @@
+let thirdPlanet = "Earth"
+
+var ninthPlanet: String
+
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+
+var sixthPlanet: String = "Saturn"
+
+let seventhPlanet = "Uranus"
+
+let numberOfCountriesInAfrica = 54
+
+sixthPlanet = "Saturn"
+
+let numberOfMoonsOfSaturn = 62
