1872d54f13ff14c4c71c5f55ec3130a65543fdff
Arun Mavumkal <arunmavumkal@Aruns-MBP.fios-router.home>
2016-09-05 01:02:46 -0400

    Complete

---
+++ DataTypesPlaygrounds.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//string constant
+let thirdPlanet = "Earth"
+
+// string variable
+var ninthPlanet: String
+
+// no longer a planet
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet s constant of type String. 
+
+let sixthPlanet: String = "Saturn"
+// sixthPlanet is a constant of type string
+
+let seventhPlanet = "Uranus"
+//seventhPlanet is a constant of type string
+
+let numberOfCountriesInAfrica = 54
+// numberOfCountriesInAfrica is a constant of type integer 
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
+
