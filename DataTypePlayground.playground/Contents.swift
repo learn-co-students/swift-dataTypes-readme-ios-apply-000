87b1e9a952962def52acfc039ef8331db230177f
Behdad Analui <behdad@Behdads-iMac.local>
2016-09-17 20:07:35 -0700

    Added playground file

---
+++ DataTypePlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let thirdPlanet = "Earth"
+var ninethPlanet : String
+ninethPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet is a constant of type String
+
+let sixthPlanet: String = "Saturn"
+// sixthPlanet is a constant of type String
+
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant of type String
+
+let numberOfCountriesInAfrica = 54
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
