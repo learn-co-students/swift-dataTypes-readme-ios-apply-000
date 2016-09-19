191fc637a432ea6f0fef8c6f530bd89b4ecbb7d2
Serg T <sergtsaeb@gmail.com>
2016-09-19 12:13:02 -0700

    data types practice

---
+++ DataTypes.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+let ThirdPlanet = "Earth"
+
+var ninthPlanet : String
+
+ninthPlanet = "Pluto"
+
+
+let fifthPlanet: String = "Jupiter"
+
+let sixthPlanet: String = "Saturn"
+
+let seventhPlanet = "Uranus"
+
+
+let numberOfCountriesInAfrica = 54
+
+
+let numberOfMoonsOfSaturn = 62
+
+var saturnMoons = "There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)."
+
+print(saturnMoons)
+
