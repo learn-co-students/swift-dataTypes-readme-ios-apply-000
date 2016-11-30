108d62ccec28a814b2024a88df3e2e4373af524d
David Graves <dbgraves120@gmail.com>
2016-11-30 01:14:38 -0500

    Completed

---
+++ Var Playground.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+let thirdPlanet = "Earth"
+var ninthPlanet: String
+
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+// fifth planet is a constant of type String
+let sixthPlanet: String = "Saturn"
+// sixthPlanet is a constant of type String
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant of type String
+
+let numberOfCountriesInAfrica = 54
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting Saturn.")
+
+
