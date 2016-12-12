ba736d199a632869ae047d40091a4e565c9bbc4f
Bakhti <bakhti@Bakhtis-MacBook-Air.local>
2016-12-12 15:44:02 -0500

    DataTypesStringsInt

---
+++ StringsAndIntegers.playground/Contents.swift
@@ -0,0 +1,18 @@
+let thirdPlanet = "Earth"
+var ninthPlanet : String
+ninthPlanet = "Pluto"
+
+
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet is a constant of type string
+
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant of type string
+
+let numberOfCountriesInAfrica = 54
+
+let sixthPlanet = "Saturn"
+let numberOfMoonsOfSaturn = 62
+
+print ("there are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)" )
+
