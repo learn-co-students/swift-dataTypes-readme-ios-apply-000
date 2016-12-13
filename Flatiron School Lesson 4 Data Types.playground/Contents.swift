167dfefe9dc768cf726ac9845a97d807db734799
Jonathan Shitrit <jonathanshitrit@Jonathans-MBP.fios-router.home>
2016-12-12 22:33:58 -0500

    Teaching Swift about astronomy

---
+++ Flatiron School Lesson 4 Data Types.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let thirdPlanet = "Earth"
+
+var ninthPlanet: String
+
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet is a constant of type String
+
+let sixthPlanet: String = "Saturn"
+// sixthPlanet is a constant of type String
+let numberOfMoonsOfSaturn = 62
+
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant of type String
+
+let numberOfCountriesInAfrica = 54
+
+print("There are \(numberOfMoonsOfSaturn) of moon orbiting \(sixthPlanet).")
