48f8daf2da30993df19e9e3cb34deeb6c6a80955
Meggie Smith <oleary.audio@gmail.com>
2016-12-17 12:47:50 -0500

    data type playground added

---
+++ dataTypes.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var ninthPlanet: String
+ninthPlanet = "Pluto"
+
+let thirdPlanet = "Earth"
+
+let fifthPlanet: String = "Jupiter"
+// string
+
+let sixthPlanet: String = "Saturn"
+// string
+
+let seventhPlanet = "Uranus"
+// string
+
+let numberOfCountriesInAfrica = 54
+
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) orbiting \(sixthPlanet)")
