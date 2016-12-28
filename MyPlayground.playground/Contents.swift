41280cbf97191df99adf1dc3c184341cd4a93266
almusto <ales.musto@gmail.com>
2016-12-28 12:58:52 -0500

    moons of Saturn

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let thirdPlanet = "Earth"
+
+var ninthPlanet: String
+
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet is a constant of type String
+
+let sixthPlanet: String = "Saturn"
+// sixthPlanet is a constant of type String
+
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant of type String
+
+let numberOfCountriesInAfrica = 54
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
