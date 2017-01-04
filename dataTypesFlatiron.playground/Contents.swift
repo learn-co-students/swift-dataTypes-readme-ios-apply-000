1e7a2e06abc3e39332a4ebf595bf9de6c8c62b4e
Michael Florez <mike_florez@ymail.com>
2017-01-04 15:43:14 -0700

    done

---
+++ dataTypesFlatiron.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let thirdPlanet = "Earth"
+
+var ninthPlanet: String
+
+ninthPlanet = "Pluto"
+let fifthPlanet: String = "Jupiter"
+
+let sixthPlanet: String = "Saturn"
+
+let seventhPlanet: String = "Uranus"
+
+let numberOfCountriesInAfrica = 54
+
+let numberOfMoonsOfSaturn = 62
+
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet).")
