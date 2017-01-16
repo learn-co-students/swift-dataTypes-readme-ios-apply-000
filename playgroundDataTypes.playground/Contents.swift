095aaed47b6a52645f09cf1e6873b7d47df673f6
Oliver-Moffat <oliver.locke.moffat@gmail.com>
2017-01-16 11:27:47 -0500

    add Playground file

---
+++ playgroundDataTypes.playground/Contents.swift
@@ -0,0 +1,13 @@
+let thirdPlanet = "Earth"
+
+let fifthPlanet: String = "Jupiter"
+
+let sixthPlanet: String = "Saturn"
+let numberOfMoonsOfSaturn = 62
+
+let seventhPlanet = "Uranus"
+
+let numberOfCountriesInAfrica = 54
+
+
+print("there are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
