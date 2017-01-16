6f2ca4d03a3339e5661a896aaec5b5dc438f4764
Michael Watanabe <mikey.watanabe@gmail.com>
2017-01-16 16:16:48 -0500

    Playground Lesson 3

---
+++ Playground Lesson 3.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let thirdPlanet = "Earth"
+
+var ninthPlanet: String
+
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet is a constant type String
+
+let sixthPlanet: String = "Saturn"
+// sixthPlanet is a constant type String
+
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant type String
+
+let numbersOfCountriesInAfrica = 54
+
+let numbersOfMoonsOfSaturn = 62
+
+print("\(sixthPlanet) has \(numbersOfMoonsOfSaturn) moons.")
+
+
+
+
+
+
+
