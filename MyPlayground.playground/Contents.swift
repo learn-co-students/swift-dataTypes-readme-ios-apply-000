06f4fd78f385837db9fa0850ed207986b97b5d41
tehcoyote <ohnap@hotmail.com>
2017-04-09 19:26:56 -0400

    fourth lab finished

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var num = 23
+var num1 = 2.2
+
+var str1:String
+var num3:Int
+
+num3 = 34535
+
+let S_SIXTH_PLANET = "Saturn"
+let I_NUMBER_OF_MOONS_OF_SATURN = 62
+print(str + " yo \(num) \(num1)")
+print("There are \(I_NUMBER_OF_MOONS_OF_SATURN) moons orbiting \(S_SIXTH_PLANET)")
