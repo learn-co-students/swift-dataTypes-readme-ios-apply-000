2f875dc4bc76d05795d83c0dd6789c34ecfa69d2
“Diljeet <jeetds1994@gmail.com>
2017-04-11 20:02:50 -0400

    added playground as requested

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let thirdPlanet = "Earth"
+
+var s = "my string"
+
+var ninthPlanet: String
+ninthPlanet = "Pluto"
+
+
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet is a constant of type String
+let sixthPlanet: String = "Saturn"
+// sixthPlanet is a constant of type String
+
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant of type String
+
+let numberOfCountriesInAfrica = 54
+
+let numberOfMoonsOfSaturn = 62
+
+//Can you print out the sentence "There are 62 moons orbiting Saturn" using string interpolation? Check this lesson's playground if you get stuck!
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
